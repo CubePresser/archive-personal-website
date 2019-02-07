ab4c1e542b593f6dd38a09f72d25f5069ed195ac
Jonathan (Alex) Jones <jonesjon@oregonstate.edu>
2019-02-06 17:36:46 -0800

    Created template for rooms

---
+++ src/scripts/scenes/template.js
@@ -0,0 +1,16 @@
+/**
+ * @author jonesjonathan
+ */
+
+import THREE from '../three';
+import {Room} from './room';
+
+const SETTINGS = {
+    
+};
+
+class Template {
+    constructor(renderer, camera) {
+        super(renderer, camera);
+    }
+};
